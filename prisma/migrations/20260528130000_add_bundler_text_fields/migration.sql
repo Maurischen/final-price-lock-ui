b4e9a7475ba3598f968a89f5c224d52e1a209d0c
Maurischen <admin@bagodicks.co.za>
2026-05-28 14:06:01 +0200

    Add bundler text fields

---
+++ prisma/migrations/20260528130000_add_bundler_text_fields/migration.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "BundlerRule" ADD COLUMN "badgeText" TEXT DEFAULT 'Bundle & save';
+ALTER TABLE "BundlerRule" ADD COLUMN "headlineText" TEXT DEFAULT 'Bundle these essentials and save instantly';
